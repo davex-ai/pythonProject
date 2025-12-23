13b9adf5d799dc7d39b47006878b8c44c40cf7d7
davex-ai <omotoyedave4@gmail.com>
2025-12-24 00:39:39 +0100

    functions & dictionaries.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+
+
+def getchoices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    return player_choice
+
+def greeting():
+    print("Hi")
+
+
+greeting()
+choices = getchoices()
+print(choices)
