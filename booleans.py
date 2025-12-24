4591da44f535365e70dd6c2a8f444501c29fb413
davex-ai <omotoyedave4@gmail.com>
2025-12-24 03:08:54 +0100

    booleans - booleans.py

---
+++ booleans.py
@@ -0,0 +1,14 @@
+# from assignment import is_coding
+
+
+# if is_coding:
+#     print("You're coding")
+# else:
+#     print("Not coding")
+
+
+
+seen = True
+not_seen = False
+print(any([seen,not_seen]))
+print(all([seen,not_seen]))
