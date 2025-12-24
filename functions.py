eda555dfc77738d031f558656d4bfa4fbbcc8770
davex-ai <omotoyedave4@gmail.com>
2025-12-24 01:15:38 +0100

    concatenation

--- functions.py
+++ functions.py
@@ -3,15 +3,18 @@ import random
 
 def getchoices():
     print("Welcome To the RPS Game🎉🎉")
-    player_choice = input("Rock, Paper, Scissors: ")
+    player_choice = input("rock, paper, scissors: ")
     opt = ['Rock', 'Paper', 'Scissors']
     computer_choice = random.choice(opt)
     choices = { "player" : player_choice, "computer": computer_choice }
+    check_winner(player_choice, computer_choice)
     return choices
 
 def check_winner(player, computer):
+    print("Your choice: " + player + ", Computer Choice: " + computer)
     if player == computer:
-        return "Its a tie"
+        return "Its a tie p🎉"
+
 def greeting():
     print("Hi")
 
