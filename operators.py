244921b8923d561276dcb897023a0403ce0da831
davex-ai <omotoyedave4@gmail.com>
2025-12-24 02:31:47 +0100

    Operators - operators.py

---
+++ operators.py
@@ -0,0 +1,38 @@
+# Arithmetic Operators
+from assignment import is_coding
+
+print("1. Arithmetic Operators ")
+
+print(1 + 2)
+print(5 - 4)
+print(2 * 2)
+print(16 / 8)
+print(12 % 4)
+print(8 ** 2)
+print(9 // 2)
+
+# Comparison Operators
+
+print("\n2. Comparison Operators")
+
+num1 = 2
+num2 = 4
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 <= num2)
+
+# Logical Operators
+print("\n3. Logical Operators")
+
+true = True
+false = False
+
+print(not true)
+print(true and false)
+print(true or false)
+
+#Tenary Operator
+print("\n3. Tenary Operators")
+print(True if is_coding else false)
